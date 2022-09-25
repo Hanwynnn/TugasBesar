3330e470ddff1536c0f67582fa6395c1f9a3dca0
CroZie19 <willychristianchia12@gmail.com>
2022-09-25 14:38:42 +0700

    ugd

--- app/src/main/java/com/willychia/myapplication/EditFilmActivity.kt
+++ app/src/main/java/com/willychia/myapplication/EditFilmActivity.kt
@@ -3,22 +3,27 @@ package com.willychia.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import com.willychia.myapplication.Room.BigDB
 import com.willychia.myapplication.Room.Constant
 import com.willychia.myapplication.Room.RoomFilm.NoteFilm
 import kotlinx.android.synthetic.main.activity_edit_film.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import java.lang.Float
+
 
 class EditFilmActivity : AppCompatActivity() {
     val db by lazy { BigDB(this) }
     private var noteId: Int = 0
+    private var button_update: Button?=null
+    private var button_save: Button?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit)
+        setContentView(R.layout.activity_edit_film)
+
+        button_save = findViewById(R.id.button_save)
+        button_update = findViewById(R.id.button_update)
+
         setupView()
         setupListener()
 
@@ -29,48 +34,43 @@ class EditFilmActivity : AppCompatActivity() {
         val intentType = intent.getIntExtra("intent_type", 0)
         when (intentType){
             Constant.TYPE_CREATE -> {
-                button_update.visibility = View.GONE
+                button_update?.visibility = View.GONE
             }
             Constant.TYPE_READ -> {
-                button_save.visibility = View.GONE
-                button_update.visibility = View.GONE
+                button_save?.visibility = View.GONE
+                button_update?.visibility = View.GONE
                 getNote()
             }
             Constant.TYPE_UPDATE -> {
-                button_save.visibility = View.GONE
+                button_save?.visibility = View.GONE
                 getNote()
             }
         }
     }
     private fun setupListener() {
-        button_save.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
+        button_save?.setOnClickListener {
                 db.filmDAO().addNoteFilm(
                     NoteFilm(0,edit_judul.text.toString(),
                         edit_genre.text.toString(),
                         edit_rating.text.toString().toFloat())
                 )
                 finish()
-            }
         }
-        button_update.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                NoteFilm(0,edit_judul.text.toString(),
-                    edit_genre.text.toString(),
-                    edit_rating.text.toString().toFloat())
-
+        button_update?.setOnClickListener {
+                db.filmDAO().updateNoteFilm(
+                    NoteFilm(noteId,edit_judul.text.toString(),
+                        edit_genre.text.toString(),
+                        edit_rating.text.toString().toFloat())
+                )
                 finish()
-            }
         }
     }
     fun getNote() {
         noteId = intent.getIntExtra("intent_id", 0)
-        CoroutineScope(Dispatchers.IO).launch {
             val notes = db.filmDAO().getNote(noteId)[0]
             edit_judul.setText(notes.judul)
             edit_genre.setText(notes.genre)
             edit_rating.setText(notes.rating.toString())
-        }
     }
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
